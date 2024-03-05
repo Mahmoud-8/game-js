e5885c558d3bef0ea59ccbe546ff42a712f199d7
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-05 14:36:30 +0200

    done function in js

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
-const board = document.getElementById('game-board');
-
+const board = document.getElementById("game-board");
 
 const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
+let direction = "right";
 
 function draw() {
-    board.innerHTML = '';
-    drawSnake();
+  board.innerHTML = "";
+  drawSnake();
+  drawFood();
 }
 
-function drawSnake() {      
-    snake.forEach((segment) => {
-        const snakeElement = createGameElement('div', 'snake');
-        setPosition(snakeElement, segment);
-        board.appendChild(snakeElement);
-    })
+function drawSnake() {
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
+  });
 }
 
 function createGameElement(tag, className) {
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
 }
 
 function setPosition(element, position) {
-    element.style.gridColumn = position.x;
-    element.style.gridRow = position.y;
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
 }
 
-// draw();
+draw();
 
 function drawFood() {
-    const foodElement = createGameElement('div', 'food');
-    setPosition(foodElement, food);
-    board.appendChild(foodElement);
+  const foodElement = createGameElement("div", "food");
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
 }
 
 function generateFood() {
-    const x = Math.random() * gridSize;
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case "up":
+      head.y--;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "right":
+      head.x++;
+      break;
+  }
 }
