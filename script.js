a737d13a9229aa30b124ef897523f478968ba50e
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-06 20:08:06 +0200

    done function in js

--- script.js
+++ script.js
@@ -66,4 +66,26 @@ function move() {
   }
     snake.unshift(head);
 
- 
+    // snake.pop();
+    if (head.x === food.x && head.y === food.y) {
+      food = generateFood();
+      clearInterval();
+      gameInterval = setInterval(() => {
+        move();
+        draw();
+      },gameSpeedDelay);
+    } else {
+      snake.pop();
+    }
+}
+
+// setInterval(() => {
+//     move();
+//   draw();
+// }, 200);
+
+
+function startGame() {
+    gameStarted = true;
+
+}
